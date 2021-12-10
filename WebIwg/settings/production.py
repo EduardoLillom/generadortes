5cd59a323894401b2bf4e9a6651cbd1c8445d495
EduardoLillom <86894735+EduardoLillom@users.noreply.github.com>
2021-12-10 12:43:46 -0300

    Update production.py

--- WebIwg/settings/production.py
+++ WebIwg/settings/production.py
@@ -10,11 +10,8 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db',
-        'USER': 'db',
-        'PASSWORD': '65f5czWoKSXDJ6X6',
-        'HOST': 'app-fdd22719-7cdd-4fd1-ba9e-21890d584c77-do-user-10205479-0.b.db.ondigitalocean.com',
-        'DATABASE_PORT': '25060',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db_sqllite3',
+
     }
 }
