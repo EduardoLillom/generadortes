b36dca1e6900b74029eaf9aa4b44504bf2b687fb
EduardoLillom <86894735+EduardoLillom@users.noreply.github.com>
2021-12-10 14:48:38 -0300

    arreglando variables

--- WebIwg/settings/local.py
+++ WebIwg/settings/local.py
@@ -4,17 +4,14 @@ from .base import *
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db',
-        'USER': 'db',
-        'PASSWORD': '65f5czWoKSXDJ6X6',
-        'HOST': 'app-fdd22719-7cdd-4fd1-ba9e-21890d584c77-do-user-10205479-0.b.db.ondigitalocean.com',
-        'DATABASE_PORT': '25060',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+
     }
 }
