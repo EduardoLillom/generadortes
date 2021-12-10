fe7f589d9428365c14599a67539494c137c68ef2
EduardoLillom <86894735+EduardoLillom@users.noreply.github.com>
2021-12-10 18:05:26 -0300

    credenciales

--- WebIwg/settings/production.py
+++ WebIwg/settings/production.py
@@ -10,8 +10,12 @@ ALLOWED_HOSTS = ['generadorcertamen.herokuapp.com']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd567mtddg647va',
+        'USER': 'glrbposahzvxxq',
+        'PASSWORD': '8ae46b76e25c0a13d9c9fc9934a94b1673eb87de50e14e9ce028fb8ef9801ddc',
+        'HOST': 'ec2-3-211-228-251.compute-1.amazonaws.com',
+        'PORT': 5432,
 
     }
 }
